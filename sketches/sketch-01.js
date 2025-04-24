5b620706e899587cfc5143c22b4ca4f001c6fcad
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-04-25 01:54:22 +0300

    Assignment 1 Canvas Domestika complete

--- sketches/sketch-01.js
+++ sketches/sketch-01.js
@@ -4,49 +4,43 @@
 
 // Check canvas Sketch documentation
 
-const canvasSketch = require("canvas-sketch");
+const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: "A4",
+  dimensions: [ 1080, 1080 ]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = "white";
+    context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
+    context.lineWidth = width * 0.01;
 
-    const w = 60;
-    const h = 60;
-    const gap = 20;
+    const w 	= width  * 0.10;
+    const h 	= height * 0.10;
+    const gap = width  * 0.03;
+    const ix 	= width  * 0.17;
+    const iy 	= height * 0.17;
+
+    const off = width  * 0.02;
+
     let x, y;
+
     for (let i = 0; i < 5; i++) {
       for (let j = 0; j < 5; j++) {
-        x = 100 + (w + gap) * i;
-        y = 100 + (h + gap) * j;
+        x = ix + (w + gap) * i;
+        y = iy + (h + gap) * j;
 
         context.beginPath();
         context.rect(x, y, w, h);
+        context.strokeStyle = 'white'
         context.stroke();
 
         if (Math.random() > 0.5) {
           context.beginPath();
-          context.rect(x + 8, y + 8, w - 16, h - 16);
-        context.strokeStyle = 'blue'
-          context.stroke();
-          
-          context.beginPath();
-          context.arc(x + w / 2, y + h / 2, 10, 0, Math.PI * 2);
-        context.strokeStyle = 'purple'
+          context.rect(x + off / 2, y + off / 2, w - off, h - off);
           context.stroke();
         }
-        context.beginPath();
-        context.arc(x + w / 2, y + h / 2, 19, 0, Math.PI * 2);
-        context.stroke();
-        
-        context.beginPath();
-        context.arc(x + w / 2, y + h / 2, 5, 0, Math.PI * 2);
-        context.strokeStyle = 'red'
-        context.stroke();
       }
     }
   };
